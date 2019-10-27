b5d3bc96966380812c14f061cc5cce97f3ecd05d
benjamingomez0 <benjamingomez0@gmail.com>
2019-10-26 19:40:58 -0700

    added homepage route and added middleware

---
+++ db/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost/proj2'
+
+//connecting to db
+
+mongoose.connect(connectionString,{ useNewUrlParser: true,
+                                    useUnifiedTopology: true,
+                                    useCreateIndex: true,
+                                    useFindAndModify: false
+   });
+
+//connection checks
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${connectionString}`);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.log('Mongoose error: ', err);
+  });
+  
+
+
--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require('express');
-const app = express();
-const PORT = 3030;
+const app     = express();
+const PORT    = 3030;
+
+//middleware requires
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+const session        = require('express-session')
+
+// connecting db to server
+require('./db/db')
+
+//session middleware
+app.use(session({
+        secret: "two may keep council putting one away",
+        resave: false, 
+        saveUninitialized: false 
+}));
+
+//middleware
+app.use(methodOverride('_method'));//must come before our routes
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 
+//homepage
+
+app.get('/',function(req,res){
+    try{
+        res.render('index.ejs');
+    }
+    catch(err)
+    {
+        res.send(err)
+    }
+
+});
 
 
 
